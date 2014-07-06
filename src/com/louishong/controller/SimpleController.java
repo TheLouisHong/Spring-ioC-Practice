410ca6f9de5afb08780079b5dd67b2903781c630
Louis Hong <honglouis@outlook.com>
2014-07-06 21:01:18 +0800

    Initial

---
+++ src/com/louishong/controller/SimpleController.java
@@ -0,0 +1,24 @@
+package com.louishong.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.louishong.domain.Person;
+
+@RestController
+@RequestMapping("/person")
+public class SimpleController {
+
+	@RequestMapping("/{name}")
+	private Person getPerson(
+			@RequestParam(value = "name", required = false, defaultValue = "Fucking Nobody") String name) {
+
+		Person person = new Person();
+		person.setAge(13213);
+		person.setName(name);
+
+		return person;
+	}
+}
